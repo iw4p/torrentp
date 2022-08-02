7aaf07f3d52681765bd7bcbd92fd88189a8f275e
Nima Akbarzade <Nimakbarzade@yahoo.com>
2022-08-03 00:23:02 +0430

    fix calling status.name before initing

--- torrent.py
+++ torrent.py
@@ -11,8 +11,8 @@ class Session():
         self._download_rate_limit: int = 0
     
     def create_session(self):
-        return lt.session({'listen_interfaces': '0.0.0.0:6881'})
-        # return lt.session({'listen_interfaces':f'{self._listen_interfaces}:{self._port}'})
+        # return lt.session({'listen_interfaces': '0.0.0.0:6881'})
+        return lt.session({'listen_interfaces':f'{self._listen_interfaces}:{self._port}'})
         
     def __str__(self):
         pass
@@ -43,7 +43,6 @@ class Torrent_info():
         pass
     
     def __call__(self):
-        # return lt.torrent_info(self._path)
         return self.create_torrent_info()
 
 class Downloader():
@@ -54,14 +53,13 @@ class Downloader():
         self._file = None
         self._status = None
         self._name = ''
-        self._statistics = {'progress': 0, 'download_rate': 0, 'upload_rate': 0, 'num_peers': 0}
+        # self._statistics = {'progress': 0, 'download_rate': 0, 'upload_rate': 0, 'num_peers': 0}
         self._state = ''
 
 
     def status(self):
         if self._save_path == None:
             self._save_path = '.'
-        print(self._torrent_info)
         self._file = self._session.add_torrent({'ti': self._torrent_info, 'save_path': f'{self._save_path}'})
         self._status = self._file.status()
 
@@ -72,26 +70,17 @@ class Downloader():
         self._name = self.status().name
         return self._name
 
-    def start_download(self):
+    def download(self):
+        print(f'Start downloading {self.name}')
         while (not self._status.is_seeding):
+            
             s = self.status()
-            self._statistics['progress'] = s.progress
-            self._statistics['download_rate'] = s.download_rate
-            self._statistics['upload_rate'] = s.upload_rate
-            self._statistics['num_peers'] = s.num_peers
-            self._statistics['state'] = s.state
 
             print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
                 s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
                 s.num_peers, s.state), end=' ')
             
-            # alerts = self._session.pop_alerts()
-            # for a in alerts:
-            #     if a.category() & lt.alert.category_t.error_notification:
-            #         print(a)
-
-            # sys.stdout.flush()
-
+            sys.stdout.flush()
             time.sleep(1)
 
         print(self._status.name, 'downloaded successfully.')        
@@ -105,13 +94,28 @@ class Downloader():
     def __call__(self):
         pass
 
-session1 = Session()
+class Torrent_downloader():
+    def __init__(self, file_path, save_path):
+        self._file_path = file_path
+        self._save_path = save_path
+        self._session = Session()
+        self._torrent_info = Torrent_info(self._file_path)
+        self._downloader = Downloader(self._session(), self._torrent_info(), self._save_path)
+        self._file = None
 
-t_info = Torrent_info('./4FBB8AB86F4E3A9F6B76F824CAA25C9F93C58125.torrent')
-print(t_info())
+    def start_download(self):
+        self._file = self._downloader
+        self._file.download()
 
-downloadable_file = Downloader(session1(), t_info(), '.')
+    def __str__(self):
+        pass
 
-print(downloadable_file.name)
-downloadable_file.start_download()
+    def __repr__(self):
+        pass
 
+    def __call__(self):
+        pass 
+
+
+torrent_file = Torrent_downloader('4FBB8AB86F4E3A9F6B76F824CAA25C9F93C58125.torrent', '.')
+torrent_file.start_download()
